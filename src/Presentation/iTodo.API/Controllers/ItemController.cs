020dd2fc356aee5a52bb0415f1061b096fc5ec8a
Tran Quoc Hoan <dev.tranquochoan@gmail.com>
2023-02-21 17:22:14 +0700

    Add solution structure.

---
+++ src/Presentation/iTodo.API/Controllers/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace iTodo.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class ItemController : ControllerBase
+{
+
+	public ItemController()
+	{
+
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetItems()
+	{
+		var items = new List<string>
+		{
+			"Item 1", "Item 2"
+		};
+
+		return Ok(await Task.FromResult(items));
+	}
+}
+
---
+++ src/Presentation/iTodo.API/Program.cs
@@ -0,0 +1,28 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+#region Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+#endregion
+
+var app = builder.Build();
+
+#region Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    
+}
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+#endregion
