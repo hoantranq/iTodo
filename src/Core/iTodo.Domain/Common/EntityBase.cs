adb703a6e60765a7b728ec9dc93bde70ff2850d2
Tran Quoc Hoan <dev.tranquochoan@gmail.com>
2023-02-21 22:59:04 +0700

    Add EntityBase, Item entities in Domain project.

---
+++ src/Core/iTodo.Domain/Common/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿namespace iTodo.Domain.Common;
+
+public class EntityBase<T>
+{
+	public T Id { get; set; } = default!;
+
+	public string? CreatedBy { get; set; }
+
+	public string? UpdatedBy { get; set; }
+
+	public DateTime? CreatedOn { get; set; }
+
+	public DateTime? ModifiedOn { get; set; }
+}
+
---
+++ src/Core/iTodo.Domain/Entities/Item.cs
@@ -0,0 +1,10 @@
+﻿using iTodo.Domain.Common;
+
+namespace iTodo.Domain.Entities;
+
+public class Item : EntityBase<Guid>
+{
+    public string? Title { get; set; }
+    public bool IsDone { get; set; }
+}
+
